4d8cc7c026eb6a5949f30533465c0f6fd2bb10bd
Sofie McComb <smccomb@esm.ucsb.edu>
2019-02-05 15:58:13 -0800

    Shiny dashboard additions

--- Oakology/app.R
+++ Oakology/app.R
@@ -1,50 +1,24 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#Oakology Shiny App
+# Find out more about building applications with Shiny here: http://shiny.rstudio.com/
 
 library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+#Define the UI for the application
+ui <- dashboardPage(
+  dashboardHeader(title="Oakology"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Summary", tabName = "Summary", icon=icon("tree deciduous"), lib="glyphicon"),
+      menuItem("Islands", tabName="Islands",icon=icon("globe"), lib="glyphicon"),
+      menuItem("SDM", tabName = "SDM", icon=icon("leaf"), lib="glyphicon")
+    ) #sidebarmenu end
+  ), #sidebar end
+  dashboardBody()
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+#Define server logic
+server <- function(input, output) { }
 
-# Run the application 
+#Run the application
 shinyApp(ui = ui, server = server)
-
