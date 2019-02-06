794c0151eda6c547d68d6d4ef27a477577f95c71
Sofie McComb <smccomb@ESM.UCSB.EDU>
2019-02-05 17:21:43 -0800

    Added leaflet portion

--- Oakology/app.R
+++ Oakology/app.R
@@ -1,104 +1,63 @@
 #Oakology Shiny App
 # Find out more about building applications with Shiny here: http://shiny.rstudio.com/
 
-# library(shiny)
-# library(shinydashboard)
-# 
-# #Define the UI for the application
-# 
-# ui <- dashboardPage(
-#   dashboardHeader(title="Oakology"),
-#   dashboardSidebar(
-#     sidebarMenu(
-#       menuItem("Summary", tabName = "summary", icon=icon("tree deciduous"), lib="glyphicon"),
-#       menuItem("Islands", tabName="islands",icon=icon("globe"), lib="glyphicon"),
-#       menuItem("SDM", tabName = "sdm", icon=icon("leaf"), lib="glyphicon")
-#     ) #sidebarmenu end
-#   ),
-#   dashboardBody(
-#     tabItems(
-#       tabItem(tabName = "Summary",
-#         h2("Dashboard tab content")
-#               
-#       ),
-#       
-#       tabItem(tabName = "islands",
-#         h2("Widgets tab content"),
-#         # Boxes need to be put in a row (or column)
-#         fluidRow(
-#           box(plotOutput("plot1", height = 250)),
-#           
-#           box(
-#             title = "Controls",
-#             sliderInput("slider", "Number of observations:", 1, 100, 50)
-#           )
-#         )
-#       )
-#     )
-#   
-#   
-#   )# end dashboard body
-# )#End UI
-# 
-# #Define server logic
-# server <- function(input, output) { 
-#   set.seed(122)
-#   histdata <- rnorm(500)
-#   
-#   output$plot1 <- renderPlot({
-#     data <- histdata[seq_len(input$slider)]
-#     hist(data)
-#   })
-#   
-#   }#end server
-# 
-# #Run the application
-# shinyApp(ui = ui, server = server)
-
-
-## app.R ##
+## Shiny App ##
 library(shiny)
 library(shinydashboard)
+library(leaflet)
+library(RColorBrewer)
+library(raster)
+#library(dplyr)
 
-#Define the UI for the application
-ui <- dashboardPage(
-  dashboardHeader(title = "Oakology"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Summary", tabName = "summary",icon=icon("tree deciduous")),
-      menuItem("Islands", tabName = "islands",icon=icon("globe")),
-      menuItem("SDM", tabName = "sdm",icon=icon("leaf"))
-    )
-  ),
-  dashboardBody(
-    tabItems(
-      # First tab content
-      tabItem(tabName = "summary",
-          h2("Oakology Group Project Summary")
-      ),
-      
-      # Second tab content
-      tabItem(tabName = "islands",
-          h2("Island Environmental Layers"),
-          fluidRow(
-            box(
-              title="Santa Cruz", width=4, solidHeader = TRUE,
-              plotOutput("plot1", height = 250)),
-            
-            box(
-              title = "Controls",
-              sliderInput("slider", "Number of observations:", 1, 100, 50)
-            )
-          )
-      ),
-      
-      # Third tab content
-      tabItem(tabName = "sdm",
-          h2("Species Distribution Models")
-      )
-      
-    )
+#Define the header, sidebar, and body for the application
+header<- dashboardHeader(title = "Oakology")
+
+sidebar<-dashboardSidebar(
+  sidebarMenu(
+    menuItem("Summary", tabName = "summary",icon=icon("tree deciduous")),
+    menuItem("Islands", tabName = "islands",icon=icon("globe")),
+    menuItem("SDM", tabName = "sdm",icon=icon("leaf"))
   )
+)
+
+body<-dashboardBody(
+  tabItems(
+    # First tab content
+    tabItem(tabName = "summary",
+        h2("Oakology Group Project Summary")
+    ),
+    
+    # Second tab content
+    tabItem(tabName = "islands",
+        h2("Island Environmental Layers"),
+        fluidRow(
+          box(
+            title="Santa Cruz", width=NULL, solidHeader = TRUE,status="primary",
+            plotOutput("plot1", height = 250)),
+          
+          box(
+            title = "Santa Rosa",width=NULL, solidHeader = TRUE,status="primary",
+            sliderInput("slider", "Number of observations:", 1, 100, 50)
+          )
+        )
+    ),
+    
+    # Third tab content
+    tabItem(tabName = "sdm",
+        h2("Species Distribution Models"),
+        fluidRow(
+          box(width=NULL, solidHeader = TRUE),
+          leafletOutput("sdmmap", height=500)
+        )#fluidrow
+    )
+    
+  )
+)
+
+ui<-dashboardPage(
+  header,
+  sidebar,
+  body
 )
 
 server <- function(input, output) {
@@ -109,6 +68,19 @@ server <- function(input, output) {
     data <- histdata[seq_len(input$slider)]
     hist(data)
   })
-}
+  
+  
+  ras<-raster("data/sdm/scr/nofog/historic.tif")
+  pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(map),
+                      na.color = "transparent")
+  
+  
+  output$sdmmap<-renderLeaflet({
+    leaflet() %>%
+      addTiles() %>% 
+      addRasterImage(ras, colors = pal, opacity = 0.8)
+
+  }) #end leaflet
+}#end server
 
 shinyApp(ui, server)
