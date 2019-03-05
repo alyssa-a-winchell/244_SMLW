390782e496f1329850a847ca18da28f133e41e21
Laura Wolf <lwolf@esm.ucsb.edu>
2019-03-05 13:12:10 -0800

    Adding historic map and formatting columns in SDM tab. Figuring out colors for island tab.

--- Oakology/app.R
+++ Oakology/app.R
@@ -15,13 +15,15 @@ library(tidyverse)
 library(sf)
 library(RColorBrewer)
 library(shinyWidgets)
+library(colorspace)
 
 setwd("G:/data/GitHub/244_SMLW/Oakology")#Set wd just for running here, the app wd includes Oakology
 
 ui<-fluidPage(theme = shinytheme("readable"),
               titlePanel("Oakology"),
               navbarPage("",
-              tabPanel("Summary", "Put in summary info"),
+              tabPanel("Summary", "jjjknjk"
+                       ),
               tabPanel("Example",
                        sidebarPanel(
                          fileInput("file", "File input:"),
@@ -52,31 +54,86 @@ ui<-fluidPage(theme = shinytheme("readable"),
                        )
               ),
               tabPanel("Islands",
-                       mainPanel(
-                         selectInput("islandvar", "Choose an Island Variable", c("DEM", "Vegetation")),
-                         leafletOutput("islandmap", width=1000, height=500)
+                       fluidRow(
+                         column(2,selectInput("islandvar", "Choose an Island Variable", c("DEM", "Vegetation"))),
+                         column(4,leafletOutput("islandmap", width=800, height=500))
                        )
+                       # sidebarPanel(
+                       #   selectInput("islandvar", "Choose an Island Variable", c("DEM", "Vegetation"))
+                       # ),
+                       # mainPanel(
+                       #  
+                       #   leafletOutput("islandmap", width=1000, height=500)
+                       # )
                        ),
+              # tabPanel("SDM",
+              #          # sidebarPanel(selectInput("histsdmcolor", "Choose a Color Palette", c("Spectral","Spectral2" ,"Viridis", "Magma")),
+              #          #              selectInput("histscenario", "Choose a Scenario", c("No Fog",
+              #          #                                                             "Fog"))),
+              #          #mainPanel(leafletOutput("histsdmmap", width=1000, height=500)),
+              #          sidebarPanel(
+              #            selectInput("histsdmcolor", "Choose a Color Palette", c("Spectral","Spectral2" ,"Viridis", "Magma")),
+              #            selectInput("histscenario", "Choose a Scenario", c("No Fog",
+              #                                                               "Fog")),
+              #            br(),
+              #            br(),
+              #            selectInput("sdmcolor", "Choose a Color Palette", c("Spectral","Spectral2" ,"Viridis", "Magma")),
+              #            selectInput("scenario", "Choose a Scenario", c("No Fog",
+              #                                                           "Constant Fog",
+              #                                                           "Fog Increase",
+              #                                                           "Fog Decrease",
+              #                                                           "Fog Elevation Threshold")),
+              #            selectInput("projection", "Choose a Projection", c("Hot-Wet",
+              #                                                               "Warm-Wet",
+              #                                                               "Warm-Dry",
+              #                                                               "Hot-Dry")), #figure out how to make historic conditional for time period
+              #            #selectInput("timeperiod", "Choose a Time Period", c("2010-2039","2040-2069", "2070-2099")),
+              #            sliderTextInput("timeperiod", "Choose a Time Period", choices = c("2010-2039", 
+              #                                                                              "2040-2069", 
+              #                                                                              "2070-2099"), animate=TRUE)
+              #          ),
+              #        
+              #          mainPanel(
+              #            leafletOutput("histsdmmap", width=1000, height=500),
+              #            leafletOutput("sdmmap", width=1000, height=500)
+              #                    )
+              #          
+              #          )
               tabPanel("SDM",
-                       mainPanel(
-                         selectInput("sdmcolor", "Choose a Color Palette", c("Spectral","Spectral2" ,"Viridis", "Magma")),
-                         selectInput("scenario", "Choose a Scenario", c("No Fog",
-                                                                        "Constant Fog",
-                                                                        "Fog Increase",
-                                                                        "Fog Decrease",
-                                                                        "Fog Elevation Threshold")),
-                         selectInput("projection", "Choose a Projection", c("Hot-Wet",
-                                                                            "Warm-Wet",
-                                                                            "Warm-Dry",
-                                                                            "Hot-Dry")), #figure out how to make historic conditional for time period
-                         #selectInput("timeperiod", "Choose a Time Period", c("2010-2039","2040-2069", "2070-2099")),
-                         sliderTextInput("timeperiod", "Choose a Time Period", choices = c("2010-2039", 
-                                                                                           "2040-2069", 
-                                                                                           "2070-2099"), animate=TRUE),
-                         leafletOutput("sdmmap", width=1000, height=500)
-                                 )
-                       
+                       fluidRow(
+                         column(2,
+                                h4("Historic"),
+                                selectInput("histsdmcolor", "Choose a Color Palette", c("Spectral","Spectral2" ,"Viridis", "Magma")),
+                                selectInput("histscenario", "Choose a Scenario", c("No Fog",
+                                                                                   "Fog"))),
+                         column(4,
+                                leafletOutput("histsdmmap", width=800, height=500))
+                       ),
+                       br(),
+                       br(),
+                       fluidRow(
+                         column(2,
+                                h4("Projected"),
+                                selectInput("sdmcolor", "Choose a Color Palette", c("Spectral","Spectral2" ,"Viridis", "Magma")),
+                                selectInput("scenario", "Choose a Scenario", c("No Fog",
+                                                                               "Constant Fog",
+                                                                               "Fog Increase",
+                                                                               "Fog Decrease",
+                                                                               "Fog Elevation Threshold")),
+                                selectInput("projection", "Choose a Projection", c("Hot-Wet",
+                                                                                   "Warm-Wet",
+                                                                                   "Warm-Dry",
+                                                                                   "Hot-Dry")), #figure out how to make historic conditional for time period
+                                #selectInput("timeperiod", "Choose a Time Period", c("2010-2039","2040-2069", "2070-2099")),
+                                sliderTextInput("timeperiod", "Choose a Time Period", choices = c("2010-2039", 
+                                                                                                  "2040-2069", 
+                                                                                                  "2070-2099"), animate=TRUE)),
+                         column(4,
+                                leafletOutput("sdmmap", width=800, height=500))
                        )
+                       
+              )
+              
               )
 )
 
@@ -130,6 +187,38 @@ server <- function(input, output) {
      
    }) #end render leaflet
      
+     
+     output$histsdmmap <- renderLeaflet({
+       
+       
+       histscen<-switch(input$histscenario,
+                    "No Fog"=histscen<-"nofog",
+                    "Fog"=histscen<-"fogconstant")
+       
+       histscr<-raster(paste0("data/sdm/scr/",histscen,"/historic.tif")) 
+       proj4string(histscr) <- CRS("+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+       
+       
+       histsri<-raster(paste0("data/sdm/sri/",histscen,"/historic.tif")) 
+       proj4string(histsri) <- CRS("+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
+       
+       histmerged<-merge(histscr, histsri)
+       
+       histsdmcol <- switch(input$histsdmcolor,
+                        "Spectral" = colorNumeric(palette = "Spectral", domain=values(histmerged), na.color = "transparent", reverse=TRUE),
+                        "Spectral2" = colorNumeric(palette = "Spectral", domain=values(histmerged), na.color = "transparent", reverse=FALSE),
+                        "Viridis" = colorNumeric(palette = "viridis", domain=values(histmerged), na.color = "transparent", reverse=TRUE),
+                        "Magma" = colorNumeric(palette = "magma", domain=values(histmerged), na.color = "transparent", reverse=TRUE))
+       
+       leaflet() %>% addTiles() %>%
+         addRasterImage(histmerged, colors = histsdmcol, opacity = 0.8) %>%
+         #setView(lng=-13388304, lat=4012916, zoom=20) %>% #Figure out how to set view extent
+         addLegend("topright", pal = histsdmcol, values = values(histmerged),
+                   title = "Suitability", 
+                   labFormat = labelFormat(transform=function(histmerged) sort (histmerged, decreasing=FALSE))) #decreasing false until can figure out how to reverse legend colors
+       
+     }) #end render leaflet
+     
      output$islandmap <- renderLeaflet({
        
        # scrdem<-raster("data/islands/scr/DEM.tif") 
@@ -151,11 +240,9 @@ server <- function(input, output) {
                         "DEM" = island <- dem,
                         "Vegetation" = island <- veg)
        
-       DEMcol <- colorNumeric(palette = "BrBG", domain=values(island), na.color = "transparent", reverse=TRUE)
-       vegcol <- colorFactor(palette = "Spectral", domain=values(island), na.color = "transparent", reverse=TRUE)
        col <- switch(input$islandvar,
-                     "DEM" = col <- DEMcol,
-                     "Vegetation" = col <- vegcol)
+                     "DEM" = col <- colorNumeric(palette = grDevices::terrain.colors(20), domain=values(island), na.color = "transparent", reverse=FALSE),
+                     "Vegetation" = col <- colorFactor(palette = "Set1", domain=values(island), na.color = "transparent", reverse=FALSE))
        
        DEMlabels<-""
        veglabels<-c(": Woodland",": Chaparral",": Coastal Scrub",": Grassland",": Riparian",": Dune",": Other")
@@ -172,7 +259,7 @@ server <- function(input, output) {
                               "Vegetation" = legendtitle <- vegtitle)
        
        leaflet() %>% addTiles() %>%
-         addRasterImage(island, colors = col, opacity = 0.8) %>% 
+         addRasterImage(island, colors = col, opacity = 0.8, method = "ngb") %>% 
          #addLegend("topright", pal = col, values = values(island))
          addLegend("topright", pal = col, values = values(island), labFormat = labelFormat(suffix = legendlabels), title = legendtitle)
                   
